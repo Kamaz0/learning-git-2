3b198b73246f3c435d093ff50cc5ab21761fbc84
Pawel Bielawski <pj.bielawski@gmail.com>
2020-04-18 18:45:34 +0200

    No i co sie gapisz?

---
+++ To_zadanie.py
@@ -0,0 +1,7 @@
+zakupy = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew", "seler", "rukola"]}
+
+for sklep, lista in zakupy.items():
+   str2 = [towar.capitalize() for towar in lista]
+   print(f"Pierwsza jest {sklep.capitalize()}, kupuję tu następujące rzeczy: {str2}")
